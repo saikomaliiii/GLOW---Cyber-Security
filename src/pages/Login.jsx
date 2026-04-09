78cc03642e0003615afb69c63d58eca686682d9a
saikomaliiii <saikomali2004@gmail.com>
2026-04-09 21:19:11 +0530

    fixed api url

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -30,7 +30,7 @@ export default function Login() {
 
   try{
 
-    const res = await fetch("http://localhost:5000/api/auth/login",{
+    const res = await fetch("https://glow-cyber-security.onrender.com/api/auth/login",{
       method:"POST",
       headers:{
         "Content-Type":"application/json"
