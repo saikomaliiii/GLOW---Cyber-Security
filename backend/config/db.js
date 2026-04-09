8cdb050e21ec1de79915dec6c94a4e5cc9156a0a
saikomaliiii <saikomali2004@gmail.com>
2026-04-09 21:15:06 +0530

    fixed mongodb connection

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,10 +2,11 @@ const mongoose = require("mongoose")
 
 const connectDB = async () => {
     try {
-        await mongoose.connect("mongodb://127.0.0.1:27017/glow")
+        await mongoose.connect(process.env.MONGO_URI)
         console.log("MongoDB connected")
     } catch (error) {
         console.log(error)
+        process.exit(1)
     }
 }
 
